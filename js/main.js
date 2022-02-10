1613daf69f330ab15db32e7df837b8926d272bc8
Guillermo Illera <gillerav01@educantabria.es>
2022-02-10 11:59:30 +0100

    Cambiado un poco el diseño

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-document.onload = function() {
-
-};
-
 function info(personaje){
     var id = personaje.id;
     if (!document.getElementById("info-container")) {
@@ -11,6 +7,9 @@ function info(personaje){
         document.getElementById("main").appendChild(infoContainer);
     } else {
         document.getElementById("info-container").innerHTML = "";
+        if (document.getElementById("informacion")) {
+            document.getElementById("informacion").remove();
+        }
     }
     var h1 = document.createElement("h1");
     var nombre = function(){
@@ -57,17 +56,23 @@ function info(personaje){
         var p = document.createElement("p");
         p.innerHTML = "Cumpleaños: " + datos.birth_year;
         infoContainer.appendChild(p);
-        var enlace = document.createElement("a");
-        enlace.id = "moreInfo";
-        enlace.className = "moreInfo";
-        enlace.href = "#masInfo";
+        var section = document.createElement("section");
+        section.id = "moreInfo";
+        section.className = "moreInfo";
         var article = document.createElement("article");
-        article.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down-circle" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffec00" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"/> <circle cx="12" cy="12" r="9" /> <line x1="8" y1="12" x2="12" y2="16" /> <line x1="12" y1="8" x2="12" y2="16" /> <line x1="16" y1="12" x2="12" y2="16" /> </svg>';
-        enlace.appendChild(article);
-        infoContainer.appendChild(enlace);
-
-        
-
+        article.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" id="masInformacion" class="icon icon-tabler icon-tabler-arrow-down-circle" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffec00" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"/> <circle cx="12" cy="12" r="9" /> <line x1="8" y1="12" x2="12" y2="16" /> <line x1="12" y1="8" x2="12" y2="16" /> <line x1="16" y1="12" x2="12" y2="16" /> </svg>';
+        infoContainer.appendChild(article);
+        document.getElementById("masInformacion").addEventListener("click", function(){
+            if (document.getElementById("informacion")) {
+                document.getElementById("informacion").remove();
+            }
+            var informacion = document.createElement("section");
+            informacion.className = "informacion";
+            informacion.id = "informacion";
+            var h1 = document.createElement("h1");
+            h1.innerHTML = "información";
+            informacion.appendChild(h1);
+            document.body.appendChild(informacion);
+        })          
     });
-
 }
